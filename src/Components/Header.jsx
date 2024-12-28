ab9134e748c10f3858e1647e49a0fc6e653f7856
jozias <aelciojoziasp@gmaio.com>
2024-12-28 17:43:05 -0300

    [Header] - Criação e estilização do Header da página

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import Dogs from '../Assets/dogs.svg?react';
 
 export default function Header() {
   return (
-    <div className={styles}>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 }
