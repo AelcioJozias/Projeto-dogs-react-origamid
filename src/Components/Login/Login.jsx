87b4fe824bb59ce4aeaf95bf59246dae81756ace
jozias <aelciojoziasp@gmaio.com>
2025-01-04 21:43:15 -0300

    [Login] - customizacoes visuais no form de login, display grid e adicionado a imagem como background no lado esquerdo
    Feito também a reponsividade da tela de login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,19 +5,22 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import UserContext from '../../../UserContext';
+import style from './Login.module.css';
 
 export default function Login() {
   const { login } = useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/criar" element={<LoginCreate />} />
-        <Route path="/perdeu" element={<LoginPasswordLost />} />
-        <Route path="/resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={style.login}>
+      <div className={style.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/criar" element={<LoginCreate />} />
+          <Route path="/perdeu" element={<LoginPasswordLost />} />
+          <Route path="/resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 }
