448100781005d6cfeaea5df5525ca61aac8a3b97
jozias <aelciojoziasp@gmaio.com>
2025-01-19 15:00:02 -0300

    [Feed] - feito a parte inicial do feed, as fotos já carregam na grid com um total de 6.
    Feito também o estilo de grid, a parte que anima ao fazer um :hover na imagem mostrando a quantidade de visualizações

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import styles from './Footer.module.css';
 export default function Footer() {
-  return <div className={styles}>Footer</div>;
+  return <div className={styles}></div>;
 }
