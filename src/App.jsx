7c38e117c7d478c098a99a5a7acfe4c1e19ce860
jozias <aelciojoziasp@gmaio.com>
2025-02-02 20:43:27 -0300

    [Footer] - adicionado o footer na página

--- src/App.jsx
+++ src/App.jsx
@@ -13,25 +13,28 @@ import UserProfile from './Components/User/UserProfile';
 import NotFound from './Components/NotFound';
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login/*" element={<Login />} />
-            <Route
-              path="conta/*"
-              element={
-                <ProtectedRoute>
-                  <User />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/photo/:id" element={<Photo />} />
-            <Route path="/profile/:user" element={<UserProfile />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <main className="appBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/photo/:id" element={<Photo />} />
+              <Route path="/profile/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+
           <Footer />
         </UserStorage>
       </BrowserRouter>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import Dogs from '../Assets/dogs-footer.svg?react';
 export default function Footer() {
-  return <div className={styles}></div>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados</p>
+    </footer>
+  );
 }
